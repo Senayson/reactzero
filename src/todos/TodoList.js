57d725e0242b718a6033a81a605c58e123a557b3
Senay <senaialema@gmail.com>
2021-10-16 01:18:05 -0400

    Hook todolist.js to redux action/state

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,14 +1,22 @@
 import React from "react";
 import TodoListItem from './TodoListItem';
+import { connect } from "react-redux";
+import { removeTodo } from "./actions";
 import './TodoList.css';
 import NewTodoForm from './NewTodoForm';
 
-const TodoList = ({todos = [{text: 'hello'}]}) => {
+const TodoList = ({todos = [{text: [], onRemovePressed}]}) => {
     return (<div className="list-wrapper">
         <NewTodoForm />
-        {todos.map(todo => <TodoListItem todo={todo} />)}
+        {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />)}
     </div>)
 }
+const mapStateToProps = state => ({
+    todos: state.todos
+})
 
+const mapDispatchToProps = dispatch => ({
+    onRemovePressed: text => dispatch(removeTodo(text))
+})
 
-export default TodoList;
+export default connect(mapStateToProps,  mapDispatchToProps)(TodoList);
