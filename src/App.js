12abfb1bc59c7b3bd422cbf96eb0eb75e3684590
Senay <senaialema@gmail.com>
2021-10-15 23:05:16 -0400

    Webpack dev server running

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+console.log('Senay')
+const App = () => {
+  return ( <div className="App">
+        <h1>
+           " Hello, World!"
+        </h1>
+    </div>)
+}
+
+export default App;
