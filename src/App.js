84faf1984aee1da57f1330602db24759a2e9f3e9
Senay <senaialema@gmail.com>
2021-10-15 23:46:09 -0400

    Integrate comps into app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
+import TodoList from './todos/TodoList';
+
 
 console.log('Senay')
 const App = () => {
   return ( <div className="App">
         <h1>
-           " Hello, World!"
+          <TodoList />
         </h1>
     </div>)
 }
